80bdb73e35527fd4c50de5b16b0f8530402c6a40
Stefano Bozzi <bozzistef@gmail.com>
2023-02-27 14:26:59 -0300

    Triggering events and nextTick

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,45 +1,60 @@
 import { mount } from "@vue/test-utils";
 
+import { nextTick } from "vue";
+
 const App = {
   data() {
     return {
       count: 0,
     };
   },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
   template: `
+    <button @click="increment">Increment</button>
     <div v-if="count % 2 === 0">Count is even.</div>
-    <div v-else>Count is odd.</div>
+    <div v-if="count % 2 !== 0">Count is odd.</div>
   `,
 };
 
 const wrapperFactory = ({ data } = { data: {} }) => {
   const appClone = { ...App }; // Shallow Clone
   return mount(appClone, {
-    data,
+    data() {
+      return data;
+    },
   });
 };
 
 describe("Testing App", () => {
-  it("should render that count is odd", () => {
+  it("should render that count is odd", async () => {
     const wrapper = wrapperFactory({
-      data() {
-        return {
-          count: 1,
-        };
+      data: {
+        count: 0,
       },
     });
+
+    wrapper.find("button").trigger("click");
+
+    await nextTick();
 
     expect(wrapper.html()).toContain("Count is odd.");
   });
 
-  it("should render that count is even", () => {
+  it("should render that count is even", async () => {
     const wrapper = wrapperFactory({
-      data() {
-        return {
-          count: 2,
-        };
+      data: {
+        count: 0,
       },
     });
+
+    wrapper.find("button").trigger("click");
+    wrapper.find("button").trigger("click");
+
+    await nextTick();
 
     expect(wrapper.html()).toContain("Count is even.");
   });
