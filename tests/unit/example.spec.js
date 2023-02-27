c65fd36e8a920f86a76cb3d2a1b8b7623b33cd54
Stefano Bozzi <bozzistef@gmail.com>
2023-02-27 13:40:42 -0300

    Working with props

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,15 +1,35 @@
 import { mount } from "@vue/test-utils";
 
 const App = {
+  props: {
+    count: {
+      type: Number,
+    },
+  },
   template: `
-    <div>Hello App</div>
+    <div v-if="count % 2 === 0">Count is even.</div>
+    <div v-else>Count is odd.</div>
   `,
 };
 
 describe("Testing App", () => {
-  it("should render App correctly", () => {
-    const wrapper = mount(App);
+  it("should render that count is odd", () => {
+    const wrapper = mount(App, {
+      props: {
+        count: 1,
+      },
+    });
 
-    expect(wrapper.html()).toBe("<div>Hello App</div>");
+    expect(wrapper.html()).toContain("Count is odd.");
+  });
+
+  it("should render that count is even", () => {
+    const wrapper = mount(App, {
+      props: {
+        count: 2,
+      },
+    });
+
+    expect(wrapper.html()).toContain("Count is even.");
   });
 });
