996b4f45d6a2f8191c94d0b643f82f15adb310f3
Stefano Bozzi <bozzistef@gmail.com>
2023-02-27 21:40:38 -0300

    Plugins (Vuex) and the Composition API

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  getters: {},
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+  },
+  actions: {},
+});
--- tests/unit/composition-api.spec.js
+++ tests/unit/composition-api.spec.js
@@ -1,49 +1,72 @@
-import { ref } from "vue";
+import TestingVuex from "@/components/TestingVuex.vue";
 import { mount } from "@vue/test-utils";
+import { createStore } from "vuex";
 
-const App = {
-  setup() {
-    const count = ref(0);
-
-    const increment = () => {
-      count.value += 1;
-    };
-
-    return {
-      count,
-      increment,
-    };
-  },
-  template: `
-    <button @click="increment">Increment</button>
-    <div v-if="count % 2 === 0">Count is even.</div>
-    <div v-if="count % 2 !== 0">Count is odd.</div>
-  `,
+const createVuexStore = () => {
+  return createStore({
+    state() {
+      return {
+        count: 0,
+      };
+    },
+    getters: {},
+    mutations: {
+      increment(state) {
+        state.count++;
+      },
+      decrement(state) {
+        state.count--;
+      },
+    },
+    actions: {},
+  });
 };
 
-const wrapperFactory = ({ data } = { data: {} }) => {
-  return mount(App, {
-    data() {
-      return data;
+const wrapperFactory = (component) => {
+  const store = createVuexStore();
+
+  return mount(component, {
+    global: {
+      plugins: [store],
     },
   });
 };
 
 describe("Testing App", () => {
   it("should render that count is odd", async () => {
-    const wrapper = wrapperFactory();
+    const wrapper = wrapperFactory(TestingVuex);
 
-    await wrapper.find("button").trigger("click");
+    await wrapper.find('button[data-action="increment"]').trigger("click");
 
     expect(wrapper.html()).toContain("Count is odd.");
   });
 
   it("should render that count is even", async () => {
-    const wrapper = wrapperFactory();
+    const wrapper = wrapperFactory(TestingVuex);
 
-    await wrapper.find("button").trigger("click");
-    await wrapper.find("button").trigger("click");
+    await wrapper.find("[data-action='increment']").trigger("click");
+    await wrapper.find("[data-action='increment']").trigger("click");
 
     expect(wrapper.html()).toContain("Count is even.");
+  });
+
+  it("should increment count by one", async () => {
+    const wrapper = wrapperFactory(TestingVuex);
+
+    const prevCount = wrapper.vm.count;
+
+    await wrapper.find('[data-action="increment"]').trigger("click");
+
+    expect(wrapper.vm.count).toBe(prevCount + 1);
+  });
+
+  it("should decrement count by one", async () => {
+    const wrapper = wrapperFactory(TestingVuex);
+
+    const prevCount = wrapper.vm.count;
+
+    await wrapper.find('[data-action="decrement"]').trigger("click");
+
+    expect(wrapper.vm.count).toBe(prevCount - 1);
   });
 });
